62415657cb3a46dbb79dcb81fd07df015ad3a005
wellflat <wellflat@gmail.com>
2020-03-01 01:19:52 +0900

    chart sample

---
+++ src/components/chart.vue
@@ -0,0 +1,20 @@
+<script>
+import { Scatter, mixins } from 'vue-chartjs';
+const {  reactiveProp } = mixins;
+
+export default {
+  extends: Scatter,
+  mixins: [reactiveProp],
+  props: {
+    options: null
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options);
+  },
+  watch: {
+    chartData() {
+      this.$data._chart.update();
+    }
+  }
+}
+</script>
